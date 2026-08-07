8ac6771fe2731def57be3db4a4ae935b11caa1f6
LuisAlvarezMtz <luisangel.alvarezmtz@gmail.com>
2026-08-06 19:09:20 -0600

    feat(order-service): add rate limiter pattern

--- ecom-order-service/src/main/java/com/luisalvarez/ecom/inventoryservice/service/InventoryService.java
+++ ecom-order-service/src/main/java/com/luisalvarez/ecom/inventoryservice/service/InventoryService.java
@@ -2,6 +2,10 @@ package com.luisalvarez.ecom.inventoryservice.service;
 
 import com.luisalvarez.ecom.inventoryservice.client.InventoryClient;
 import com.luisalvarez.ecom.inventoryservice.dto.Inventory;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
@@ -15,15 +19,21 @@ public class InventoryService {
         this.inventoryClient = inventoryClient;
     }
 
-
     @Retryable(
             retryFor = RuntimeException.class,
             maxAttempts = 3,
             backoff = @Backoff(delay = 2000)
     )
+    @RateLimiter(name = "inventoryService", fallbackMethod = "fallbackMethod")
     public Inventory getInventory(Long productId) {
         System.out.println("Calling get inventory to product: " + productId);
         return inventoryClient.getInventory(productId);
+    }
+
+
+    public Inventory fallbackMethod(Long productId, Throwable throwable) {
+        System.out.println("Fallback Method called for product: " + productId);
+        return new Inventory(productId.toString(), 0);
     }
 
 }
